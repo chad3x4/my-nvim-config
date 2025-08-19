f07145d370e8949bbc30b0207f4da4e257a367a0
chungnt4520 <chungadadad@gmail.com>
2025-08-19 11:07:13 +0700

    config: change sign and scrollbar color

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -6,7 +6,49 @@ return {
         "lewis6991/gitsigns.nvim",
     },
     config = function()
-        require("scrollbar").setup()
+        require("scrollbar").setup({
+            handle = {
+                color = "#808080",
+            },
+            marks = {
+                Cursor = {
+                    text = "•",
+                },
+                Search = {
+                    text = { "-", "=" },
+                    color = "#ffb86c",
+                },
+                Error = {
+                    text = { "-", "=" },
+                    color = "#ff5555",
+                },
+                Warn = {
+                    text = { "-", "=" },
+                    color = "#f1fa8c",
+                },
+                Info = {
+                    text = { "-", "=" },
+                    color = "#50fa7b",
+                },
+                Hint = {
+                    text = { "-", "=" },
+                    color = "#8be9fd",
+                },
+                Misc = {
+                    text = { "-", "=" },
+                    color = "#bd93f9",
+                },
+                GitAdd = {
+                    text = "▎",
+                },
+                GitChange = {
+                    text = "▎",
+                },
+                GitDelete = {
+                    text = "▁",
+                },
+            },
+        })
         require("scrollbar.handlers.gitsigns").setup()
     end,
 }
